554c054fcc4bf95ea3f6a7417f5c4c741cae250b
Sam <sam.bennett@axcelinno.io>
2020-09-25 09:52:56 -0500

    ppmt function to calculate 12 month remianing average

--- src/main/java/AppFunctions.java
+++ src/main/java/AppFunctions.java
@@ -6,9 +6,10 @@ import org.kie.api.runtime.KieRuntime;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.process.ProcessInstance;
 import org.kie.api.runtime.rule.FactHandle;
+import org.apache.poi.ss.formula.functions.Finance;
+import com.pnmac.pam.utils.Utils;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class AppFunctions {
 
@@ -46,5 +47,37 @@ public class AppFunctions {
         int num = kSession.fireAllRules();
         System.out.println(num);
         System.out.println("Completed");
+    }
+
+    private Double calculateAverage(List<Double> marks) {
+        Double sum = 0.0;
+        if(!marks.isEmpty()) {
+            for (Double mark : marks) {
+                sum += mark;
+            }
+            return sum.doubleValue() / marks.size();
+        }
+        return sum;
+    }
+
+    public Double calculateCDMonthlyAmount(Double interestRate, Integer numOfPayments, Double loanAmount) {
+        List<Double> remainingBalanceList = new ArrayList<Double>();
+        Double moveRateDecimalPlace = interestRate/100;
+        Double rateDivide = moveRateDecimalPlace/12;
+        Double ppmt = (-1*Finance.ppmt(rateDivide, 1, numOfPayments, loanAmount));
+        Double remainingBalance = loanAmount - ppmt;
+        System.out.println(remainingBalance);
+        remainingBalanceList.add(remainingBalance);
+        for (int i = 2; i <= 12; i++) {
+            ppmt = (-1*Finance.ppmt(rateDivide, i, numOfPayments, loanAmount));
+            remainingBalance = remainingBalance - ppmt;
+            System.out.println(remainingBalance);
+            remainingBalanceList.add(remainingBalance);
+        }
+
+        Double remainingBalanceAverage = calculateAverage(remainingBalanceList);
+        remainingBalanceAverage = Utils.formatDouble(remainingBalanceAverage);
+
+        return remainingBalanceAverage;
     }
 }
--- src/main/java/RunApp.java
+++ src/main/java/RunApp.java
@@ -1,4 +1,6 @@
 import com.pnmac.pam.data.model.v3.loan.Loan;
+import org.apache.poi.ss.formula.functions.Finance;
+import com.pnmac.pam.utils.Utils;
 
 public class RunApp {
 
@@ -6,11 +8,22 @@ public class RunApp {
         Loan loan = new Loan();
         loan.setUnderwritingEscrowIndicator(true);
         AppFunctions appFunctions = new AppFunctions();
-        appFunctions.runProcess(loan);
+        //appFunctions.runProcess(loan);
         //appFunctions.runWithData(loan);
         //runProcess(loan);
         //runWithData(loan);
-        System.out.println(loan.getProposedDuesAmount());
-        System.out.println(loan.getLoanIssues().getIssues().contains("12345678-90ab-cdef-ghij-klmnopqrstuv"));
+        //System.out.println(loan.getProposedDuesAmount());
+        //System.out.println(loan.getLoanIssues().getIssues().contains("12345678-90ab-cdef-ghij-klmnopqrstuv"));
+
+        Double pmt = (-1*Finance.pmt(.0275/12, 360, 152575));
+        Double ppmt = (-1*Finance.ppmt(.0275/12, 12, 360, 152575));
+        Double v = (1+(.1/12));
+        Integer t = (-(36/12)*12);
+        Double result =(1000000*(10.0/12))/(1-Math.pow(v,t));
+        System.out.println(result);
+        System.out.println(Utils.formatDouble(pmt));
+        System.out.println(Utils.formatDouble(ppmt));
+        System.out.println(2.75/100);
+        System.out.println(appFunctions.calculateCDMonthlyAmount(2.75, 360, 152575.0));
     }
 }
